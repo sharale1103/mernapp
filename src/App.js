38253c7303b6ce6466f928790346daa452039d2d
Sarthak <97030182+sharale1103@users.noreply.github.com>
2024-05-18 16:29:50 +0000

    none

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from "./screen/Home";
-import Login from "./screem/Login"
+import Login from "./screen/Login"
 import React from "react";
 import {
   BrowserRouter as Router,
