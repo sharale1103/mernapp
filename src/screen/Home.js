fda91d6a7bd033e6deb7b73cad2282d9354bb957
Sarthak <97030182+sharale1103@users.noreply.github.com>
2024-05-18 10:40:38 +0000

    mernapl

---
+++ src/screen/Home.js
@@ -0,0 +1,25 @@
+import Navbar from "../components/Navbar";
+
+import React from "react";
+import Footer from "../components/Footer";
+
+function Home() {
+    return(
+        <>
+        <div>
+ <Navbar/>
+        </div>
+        <div>
+            main
+        </div>
+        <div>
+            <Footer/>
+        </div>
+        </>
+    )
+}
+
+export default Home;
+
+
+
